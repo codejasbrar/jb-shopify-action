b0e7953728b685b3de48a5c7c097fda9e0c61b99
codejasbrar <65706996+codejasbrar@users.noreply.github.com>
2023-01-08 01:34:58 +0000

    Update action.sh

--- action.sh
+++ action.sh
@@ -5,7 +5,7 @@ echo "Installing Theme Kit"
 curl -s https://raw.githubusercontent.com/Shopify/themekit/master/scripts/install.py | sudo python
 
 echo "Configuring Theme Kit"
-theme configure --password=shptka_8799aeaef7c725cde428a327157e25c7 --store=shopera3.myshopify.com --themeid=141092454705 --dir=./ --ignored-file=config/settings_data.json --ignored-file=locales/*
+theme configure --password=$SHOPIFY_APP_API_PASSWORD --store=$SHOPIFY_STORE_URL --themeid=$SHOPIFY_THEME_ID --dir=$THEME_PATH $THEMEKIT_FLAGS
 
 echo "Deploying Theme"
 theme deploy --allow-live
